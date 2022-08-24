adf4e70a7240a17c0702954c6621707e781f2e8c
netrc <ric@netrc.com>
2022-08-24 08:15:01 -0400

    utils - ymdTimestamp

--- utils.js
+++ utils.js
@@ -6,16 +6,22 @@ const fatalErr = err => {
 
 const zeroPre = n => (n<10) ? '0'+n : n
 
+const journalMonth = ( x=(new Date()) ) => {  // year-mm
+  const m = x.getMonth() + 1 // !! zero-based
+  return `${x.getFullYear()}-${zeroPre(m)}`
+}
+
+const ymdTimestamp = ( x=(new Date()) ) => {  // year-mm-dd
+  const m = x.getMonth() + 1 // !! zero-based
+  const d = x.getDay()
+  return `${x.getFullYear()}-${zeroPre(m)}-${zeroPre(d)}`
+}
+
 const isoTimestamp = ( x=(new Date()) ) => {  // year-mm-ddThh:mm // no seconds
   const xx = x.toString()
   const m = x.getMonth() + 1 // !! zero-based
   const d = x.getDay()
   return `${x.getFullYear()}-${zeroPre(m)}-${zeroPre(d)}T${xx.substring(16,21)}`
-}
-
-const journalMonth = ( x=(new Date()) ) => {
-  const m = x.getMonth() + 1 // !! zero-based
-  return `${x.getFullYear()}-${zeroPre(m)}`
 }
 
 const projectFiles = ( p='tProj' ) => {
