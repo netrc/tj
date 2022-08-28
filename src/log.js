7e3a88667f0b39ab19fc0c219ff14ff778df0614
netrc <ric@netrc.com>
2022-08-28 15:59:56 -0400

    log: change to logtail

--- src/log.js
+++ src/log.js
@@ -1,28 +1,15 @@
 
-if (!process.env.LOGZ_API) {
-  console.error('must set LOGZ_API')
+const { Logtail } = require('@logtail/node')
+
+if (!process.env.LOGTAIL_API) {
+  console.error('must set LOGTAIL_API')
   process.exit(1)
 }
 
 var l = {}
 
-if (!l.log) {
-  l = require('logzio-nodejs').createLogger( {
-    token: process.env.LOGZ_API,
-    addTimestampWithNanoSecs: true,
-    type: 'tj'
-  } )
+if (!l.info) {
+  l = new Logtail(process.env.LOGTAIL_API)
 }
 
-//l.mob = (m, o) => l.log({message:m, param1: o})
-
 module.exports = l
-
-//l.log(`${process.argv[1]}: starting`)
-
-
-//l.sendAndClose()  !! must add to all exit points???
-
-
-// const l = require('log.js')
-// 
