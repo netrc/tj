19956558ef70cd1917ddf27fcbe8797e04ebb7fb
netrc <ric@netrc.com>
2022-08-30 12:27:07 -0400

    correct use of firstNode so done items, first of the day, show up correctly

--- tj.js
+++ tj.js
@@ -18,6 +18,7 @@ const j_add = async av => {
 const t_add = async av => {
   //console.log('t_add: ', av.restOfString)
   const rl = await dyn.list().catch( u.fatalErr )
+  //console.log('t_add', Names)
   const projTodo = await dyn.findFile( rl, rl.root_file_id,[Names.ProjectFolder, Names.currentProject], Names.TodoDocument )
   const todoContent = await dyn.t.get(projTodo.id).catch( u.fatalErr )
   const currentNode = todoContent.nodes.filter( n => n.content==Names.todoInsertNodeName ) 
@@ -96,23 +97,26 @@ const t_done = async av => {
     //console.dir(doneContent)
     const doneRoot = doneContent.nodes.filter( n => n.id=='root' )[0] // 
     let firstNode = doneContent.nodes.filter( n => n.id==doneRoot.children[0] )[0]
+    //console.log('first fn'); console.dir(firstNode)
+    let insertNode = firstNode.id
     //console.dir(firstNode)
     const ymdStr = u.ymdTimestamp()
     if (!firstNode || firstNode.content != ymdStr) {
       l.info('need to make date entry: ', ymdStr)
       firstNode = await dyn.t.insert( doneContent.file_id, ymdStr, 'root', false ).catch( u.fatalErr )
+      //console.log('2nd fn'); console.dir(firstNode)
+      if (firstNode._code == 'Ok') {
+        insertNode = firstNode.new_node_ids[0]
+      }
       //console.dir(firstNode)
     }
     const doneTitles = todoContent.nodes.map( n => nDoneToText(n) ).filter(x=>x) // just the DOne lines
     // use allDone titles
     l.debug(doneTitles)
-    //const r = await dyn.t.insertArray( doneContent.file_id, doneTitles, doneRoot.children[0], false, false, -1 ).catch( u.fatalErr )
-    const r = await dyn.t.insertArray( doneContent.file_id, doneTitles, firstNode.id, false, false, -1 ).catch( u.fatalErr )
+    const r = await dyn.t.insertArray( doneContent.file_id, doneTitles, insertNode, false, false, -1 ).catch( u.fatalErr )
     //console.dir(r)
-    if (r._code == 'Ok') {
-      // now delete the old ones
-      //console.log('to be deleted')
-      //console.dir(allDone)
+    if (r._code == 'Ok') { // now delete the old ones
+      //console.log('to be deleted'); console.dir(allDone)
       const doneIds = allDone.map( d => d.id )
       //console.dir(doneIds)
       const r2 = await dyn.t.deleteArray( todoContent.file_id, doneIds ).catch( u.fatalErr )
